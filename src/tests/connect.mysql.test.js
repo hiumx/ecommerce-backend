afc40e32ec8d7407cdcc538fdbe2633690068916
hiumx <maixuanhieu250103@gmail.com>
2024-03-23 18:06:26 +0700

    Insert 10.000.000 records into MySQL  in 1 minute

--- src/tests/connect.mysql.test.js
+++ src/tests/connect.mysql.test.js
@@ -10,12 +10,49 @@ const pool = mysql.createPool({
     database: 'shopDev'
 });
 
-pool.query('SELECT * FROM user', function(err, results) {
-    if(err) throw err;
-    console.log('Result query :: ', results);
+const batchSize = 100_000;
+const totalSize = 10_000_000;
 
-    pool.end(err => { 
+let currentId = 1;
+console.time('------TIME------');
+const insertBatch = async () => {
+    const values = [];
+
+    for (let i = 0; i < batchSize && currentId <= totalSize; i++) {
+        const name = `name-${i}`;
+        const age = i;
+        values.push([name, age]);
+        currentId++;
+    }
+
+    if(!values.length) {
+        pool.end(err => {
+            if(err) 
+                console.log('Error when insert record');
+            console.timeEnd();
+            console.log('Connection closed successfully');
+        });
+        return;
+    }
+
+    const sql = 'INSERT INTO user (name, age) VALUES ?';
+
+    pool.query(sql, [values], async (err, result) => {
         if(err) throw err;
-        console.log('Connection closed');
+        console.log(`Inserted ${result.affectedRows} records`);
+        await insertBatch();
     })
-})
+}
+
+insertBatch();
+console.time('------TIME-------');
+
+// pool.query('SELECT * FROM user', function(err, results) {
+//     if(err) throw err;
+//     console.log('Result query :: ', results);
+
+//     pool.end(err => { 
+//         if(err) throw err;
+//         console.log('Connection closed');
+//     })
+// })
